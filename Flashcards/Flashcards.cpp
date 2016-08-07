7a96006163eda914a5326453c324122f813b0f3e
Christ00pher <krzysiekzabicki@interia.pl>
2016-08-07 23:31:41 +0200

    Update Flashcards.cpp

--- Flashcards/Flashcards.cpp
+++ Flashcards/Flashcards.cpp
@@ -9,10 +9,27 @@ using std::string;
 using std::map;
 using std::endl;
 using std::ofstream;
+using std::ifstream;
 using std::ios;
+using std::pair;
 
 
-Flashcards::Flashcards() {}
+Flashcards::Flashcards()
+{
+    ifstream file;
+    string s1,s2; //auxiliary variables to store words from the file
+    file.open("flashcards.txt", ios::in);
+    while (!file.eof())
+    {
+        s1 = s2 = "";
+        file >> s1;
+        file >> s2;
+        all_words.insert(pair<string, string>(s1,s2));
+    }
+    wrong_words.clear();
+    correct_words.clear();
+    file.close();
+}
 
 Flashcards::~Flashcards()
 {
@@ -33,4 +50,6 @@ void Flashcards::SaveToFile()
 
     for (map<string, string>::iterator it = all_words.begin(); it != all_words.end(); it++)
         file << it->first << " " << it->second << endl;
+
+    file.close();
 }
