69b1f52d6b646fb4e19a42043e77fdb36aa1bfe3
Christ00pher <krzysiekzabicki@interia.pl>
2016-08-09 16:12:27 +0200

    Update main.cpp

--- Flashcards/main.cpp
+++ Flashcards/main.cpp
@@ -8,9 +8,8 @@ int main()
     Flashcards f;
     while (f.IsRunning())
     {
-        f.Menu();
-        f.HandleInput();
-        //process
+        f.Menu(); //shows MENU and gets user's choice
+        f.HandleInput(); //calls functions according to the user's choice
     }
     return 0;
 }
